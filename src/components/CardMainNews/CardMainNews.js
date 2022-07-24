3835027bbb326eff8f839e64c994be3dadbe42dc
Leonardo Diasz <leonardoduarte.multimidia@gmail.com>
2022-07-24 15:40:51 -0300

    adicionando button nas noticias principais da home

--- src/components/CardMainNews/CardMainNews.js
+++ src/components/CardMainNews/CardMainNews.js
@@ -1,14 +1,17 @@
+import { ButtonGoForNotices } from "../ButtonGoForNotices/ButtonGoForNotices"
 import NewsDescription from "../NewsDescription/NewsDescription"
 import styles from './style.module.css'
 
 const CardMainNews = ({url, title, description, subtitle}) => {
   return (
     <div className={styles.cardMainNews}>
+      <ButtonGoForNotices datas={{title, description, imgUrl: url}}>
         <NewsDescription title={title} description={description}/>  
         <div>
             <img src={url} alt={subtitle} />
             <small><a href="#">{subtitle}</a></small>
         </div>
+      </ButtonGoForNotices>
     </div>
   )
 }
